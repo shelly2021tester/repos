b2cacf2a519d26f0414496fd4cf90b242ddd36df
shelly <shelly20210804@163.com>
2021-09-08 20:05:14 +0800

    update loginname

--- testlogin.py
+++ testlogin.py
@@ -3,10 +3,10 @@ import requests
 
 
 def test_login_api():
-    url="http://newbee.pansaifei.com/api/v1/user/login"
+    url="http://139.224.113.59:9000/api/v1/user/login"
     headers = {"Content-Type": "application/json"}
     payload = {
-        "loginName": "18361104557",
+        "loginName": "13361104557",
         "passwordMd5": "25D55AD283AA400AF464C76D713C07AD"
     }
     r = requests.post(url=url, data=payload, headers=headers)
@@ -15,4 +15,4 @@ def test_login_api():
     print(token_value)
 
 if __name__=='__main__':
-    pytest.main(['-sv','repos/testlogin.py'])
+    pytest.main()
