831dd1eb04706a08b2f968167954b4c64aeaa9f1
shelly <shelly20210804@163.com>
2021-09-08 08:38:06 +0800

    delete

--- pytest_f_api/runnerapi.py
+++
@@ -1,9 +0,0 @@
-import pytest
-import subprocess
-
-
-
-pytest.main()
-
-subprocess.call('allure generate ./result/temp -o ./result/report --clean',shell=True)
-subprocess.call('allure open -h 127.0.0.1 -p 83 ./result/report',shell=True)
