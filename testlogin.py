84b8e94c93648af609030d0e79be85ee58ba0ede
shelly <shelly20210804@163.com>
2021-09-08 12:52:10 +0800

    update

--- testlogin.py
+++ testlogin.py
@@ -1,18 +1,18 @@
 import pytest
 import requests
 
-
-def test_login_api():
-    url="http://newbee.pansaifei.com/api/v1/user/login"
-    headers = {"Content-Type": "application/json"}
-    payload = {
-        "loginName": "18361104557",
-        "passwordMd5": "25D55AD283AA400AF464C76D713C07AD"
-    }
-    r = requests.post(url=url, data=payload, headers=headers)
-    json_data = r.json()
-    token_value = json_data.get('data')
-    print(token_value)
+class TestCases:
+    def test_login_api(self):
+        url="http://newbee.pansaifei.com/api/v1/user/login"
+        headers = {"Content-Type": "application/json"}
+        payload = {
+            "loginName": "13915501234",
+            "passwordMd5": "E10ADC3949BA59ABBE56E057F20F883E"
+        }
+        r = requests.post(url=url, data=payload, headers=headers)
+        json_data = r.json()
+        token_value = json_data.get('data')
+        print(f"\n打印出respons的值为：{json_data},\ntoken值为：{token_value}")
 
 if __name__=='__main__':
-    pytest.main(['-sv','repos/testlogin.py'])
+    pytest.main(['-sv','testlogin.py::TestCases'])
